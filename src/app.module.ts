5f5efe734bedfeb4938587b0d697595d3e87ec82
JulianTs0 <juliantorresssr@gmail.com>
2026-01-22 20:04:28 -0300

    delete: usless module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CommonsModule } from './commons/commons.module';
 
 @Module({
-  imports: [CommonsModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
