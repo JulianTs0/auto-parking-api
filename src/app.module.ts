52f85541e00a1d5108f9a8847619a24964715cbe
JulianTs0 <juliantorresssr@gmail.com>
2026-01-30 20:32:39 -0300

    add: global handler to main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
+import { GlobalExceptionHandler } from './commons/error/GlobalExceptionHandler';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
     imports: [AppConfigModule],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [
+        AppService,
+        {
+            provide: APP_FILTER,
+            useClass: GlobalExceptionHandler,
+        },
+    ],
 })
-export class AppModule { }
+export class AppModule {}
