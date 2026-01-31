6f20a27f0bff619577bb9cfa7ab29e8e129e1ae2
JulianTs0 <juliantorresssr@gmail.com>
2026-01-31 16:53:29 -0300

    add: new errors

--- src/commons/error/ErrorType.ts
+++ src/commons/error/ErrorType.ts
@@ -4,6 +4,16 @@ export const Errors = {
         status: 500,
         message: 'Internal error',
     },
+    MISSING_REQUIRED_FIELDS: {
+        key: 'MISSING_REQUIRED_FIELDS',
+        status: 400,
+        message: 'Missing required fields',
+    },
+    INVALID_FIELDS: {
+        key: 'INVALID_FIELDS',
+        status: 400,
+        message: 'Invalid fields',
+    },
 } as const;
 
 export type ErrorType = (typeof Errors)[keyof typeof Errors];
