01976a551ec1725e0230b285f2f8be14ad088b14
JulianTs0 <juliantorresssr@gmail.com>
2026-01-31 16:58:27 -0300

    add: GlobalConstraintHandler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
 import { GlobalExceptionHandler } from './commons/error/GlobalExceptionHandler';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { GlobalConstraintHandler } from './commons/utils/pipes/GlobalConstraintHandler';
 
 @Module({
     imports: [AppConfigModule],
@@ -13,6 +14,10 @@ import { APP_FILTER } from '@nestjs/core';
         {
             provide: APP_FILTER,
             useClass: GlobalExceptionHandler,
+        },
+        {
+            provide: APP_PIPE,
+            useClass: GlobalConstraintHandler,
         },
     ],
 })
---
+++ src/commons/utils/pipes/GlobalConstraintHandler.ts
@@ -0,0 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
+import { Errors, ServiceError } from 'src/commons';
+
+export class GlobalConstraintHandler extends ValidationPipe {
+    constructor() {
+        super({
+            whitelist: true,
+            transform: true,
+            exceptionFactory: (errors: ValidationError[]) => {
+                const allConstraints = errors
+                    .map((error) => error.constraints)
+                    .filter((constraint) => constraint !== undefined)
+                    .map((constraint) => Object.keys(constraint!))
+                    .flat();
+
+                const hasMissingFields =
+                    allConstraints.includes('isNotEmpty');
+
+                if (hasMissingFields) {
+                    throw new ServiceError(
+                        Errors.MISSING_REQUIRED_FIELDS,
+                    );
+                }
+
+                throw new ServiceError(Errors.INVALID_FIELDS);
+            },
+        });
+    }
+}
