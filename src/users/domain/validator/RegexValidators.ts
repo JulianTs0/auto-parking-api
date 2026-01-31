e26f34298ea01be15e9562097a3544d0aa2d9dab
JulianTs0 <juliantorresssr@gmail.com>
2026-01-31 16:58:40 -0300

    add: regex validators

---
+++ src/users/domain/validator/RegexValidators.ts
@@ -0,0 +1,9 @@
+export class RegexValidators {
+    public static readonly EMAIL =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    public static readonly NAME =
+        /^[\p{L}]{2,20}(?:[ '-][\p{L}]{2,20})*$/u;
+    public static readonly PASSWORD =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S{8,}$/;
+    public static readonly PHONE = /^[+]?[0-9\s\-\(\)]{7,20}$/;
+}
