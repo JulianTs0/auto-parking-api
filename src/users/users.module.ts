25a2f7dc49c79c704704d5e4749b4e00963fe96f
JulianTs0 <juliantorresssr@gmail.com>
2026-02-01 13:41:43 -0300

    add: providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { PasswordEncoderI } from './config/providers/PasswordEncoderI';
+import { BcryptEncoder } from './config/providers/BCryptEncoder';
+import { TokenHandlerI } from './config/providers/TokenHandlerI';
+import { JWTHandler } from './config/providers/JWTHandler';
 
 @Module({
     imports: [],
     controllers: [],
-    providers: [],
+    providers: [
+        {
+            provide: PasswordEncoderI,
+            useClass: BcryptEncoder,
+        },
+        {
+            provide: TokenHandlerI,
+            useClass: JWTHandler,
+        },
+    ],
 })
-export class UsersModule {}
+export class UsersModule { }
