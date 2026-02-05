e26a357df47616474100511047d5fc84b7400acd
JulianTs0 <juliantorresssr@gmail.com>
2026-02-05 17:39:46 -0300

    refactor: move services to domain layer

--- src/users/domain/services/mobile/auth-mobile.service.ts
+++ src/users/domain/services/mobile/auth-mobile.service.ts
@@ -1,7 +1,7 @@
 import { AuthReq, AuthRes, RegisterReq } from 'src/users/domain';
 import { AuthMobileServiceI } from './auth-mobile-service.interface';
 import { Injectable } from '@nestjs/common';
-import { AuthService } from '../../core/auth.service';
+import { AuthService } from '../core/auth.service';
 import { UserRepository } from 'src/users/data';
 import { ServiceError } from 'src/commons/error/service.error';
 import { Errors } from 'src/commons/error/error-type.constants';
@@ -11,7 +11,7 @@ export class AuthMobileService implements AuthMobileServiceI {
     constructor(
         private readonly authCoreService: AuthService,
         private readonly userRepository: UserRepository,
-    ) {}
+    ) { }
 
     public async auth(request: AuthReq): Promise<AuthRes> {
         return Promise.resolve({} as AuthRes);
@@ -29,11 +29,11 @@ export class AuthMobileService implements AuthMobileServiceI {
         return Promise.resolve();
     }
 
-    public async resendVerifyEmail(): Promise<void> {}
+    public async resendVerifyEmail(): Promise<void> { }
 
-    public async verifyEmail(): Promise<void> {}
+    public async verifyEmail(): Promise<void> { }
 
-    public async recoverPassword(): Promise<void> {}
+    public async recoverPassword(): Promise<void> { }
 
-    public async changePassword(): Promise<void> {}
+    public async changePassword(): Promise<void> { }
 }
--- src/users/domain/services/web/auth-web.service.ts
+++ src/users/domain/services/web/auth-web.service.ts
@@ -1,7 +1,7 @@
 import { AuthReq, AuthRes, RegisterReq } from 'src/users/domain';
 import { AuthWebServiceI } from './auth-web-service.interface';
 import { Injectable } from '@nestjs/common';
-import { AuthService } from '../../core/auth.service';
+import { AuthService } from '../core/auth.service';
 import { UserRepository } from 'src/users/data';
 
 @Injectable()
@@ -9,7 +9,7 @@ export class AuthWebService implements AuthWebServiceI {
     constructor(
         private readonly authCoreService: AuthService,
         private readonly userRepository: UserRepository,
-    ) {}
+    ) { }
 
     public async auth(request: AuthReq): Promise<AuthRes> {
         return Promise.resolve({} as AuthRes);
@@ -19,13 +19,13 @@ export class AuthWebService implements AuthWebServiceI {
         return Promise.resolve();
     }
 
-    public async resendVerifyEmail(): Promise<void> {}
+    public async resendVerifyEmail(): Promise<void> { }
 
-    public async verifyEmail(): Promise<void> {}
+    public async verifyEmail(): Promise<void> { }
 
-    public async recoverPassword(): Promise<void> {}
+    public async recoverPassword(): Promise<void> { }
 
-    public async changePassword(): Promise<void> {}
+    public async changePassword(): Promise<void> { }
 
-    public async registerEmployee(): Promise<void> {}
+    public async registerEmployee(): Promise<void> { }
 }
