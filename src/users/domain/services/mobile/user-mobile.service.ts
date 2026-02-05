afe44dd49ee09b1564246c199049ec931c6f59da
JulianTs0 <juliantorresssr@gmail.com>
2026-02-05 17:38:12 -0300

    refactor: move services to domain layer

---
+++ src/users/domain/services/mobile/user-mobile.service.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@nestjs/common';
+import { UserMobileServiceI } from './user-mobile-service.interface';
+
+@Injectable()
+export class UserMobileService implements UserMobileServiceI {}
