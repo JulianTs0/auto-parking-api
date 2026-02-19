1a207c0658b5f24b1a99a4deaeebd1bdff86af0c
JulianTs0 <juliantorresssr@gmail.com>
2026-02-19 18:27:14 -0300

    update: app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/config.module';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import {
     GlobalExceptionHandler,
     GlobalConstraintHandler,
 } from './commons';
 import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { ClsModule } from 'nestjs-cls';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-    imports: [AppConfigModule, UsersModule],
+    imports: [
+        ClsModule.forRoot({
+            global: true,
+            middleware: {
+                mount: true,
+                generateId: true,
+                idGenerator: (req: any) =>
+                    req.headers['x-request-id'] ??
+                    crypto.randomUUID(),
+            },
+        }),
+        AppConfigModule,
+        AuthModule,
+        UsersModule,
+        DatabaseModule,
+    ],
     controllers: [],
     providers: [
         {
