c0a74a8851d9ba1385d96964cda58aecb57e8479
JulianTs0 <juliantorresssr@gmail.com>
2026-02-19 18:26:20 -0300

    add: auth decorator

---
+++ src/commons/config/regex.validator.ts
@@ -0,0 +1,7 @@
+export class RegexValidators {
+    public static readonly NAME =
+        /^[\p{L}]{2,20}(?:[ '-][\p{L}]{2,20})*$/u;
+    public static readonly PASSWORD =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S{8,}$/;
+    public static readonly PHONE = /^[+]?[0-9\s\-\(\)]{7,20}$/;
+}
