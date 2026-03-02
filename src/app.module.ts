3b6dd88964e857a1c207179f96a1fa9fad3a7c7c
JulianTs0 <juliantorresssr@gmail.com>
2026-03-02 14:15:50 -0300

    fix: index imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppConfigModule } from './config/config.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import {
     GlobalExceptionHandler,
     GlobalConstraintHandler,
 } from './commons';
 import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { ClsModule } from 'nestjs-cls';
-import { DatabaseModule } from './database/database.module';
 import { TransactionalAdapterTypeOrm } from '@nestjs-cls/transactional-adapter-typeorm';
 import { ClsPluginTransactional } from '@nestjs-cls/transactional';
 import { DataSource } from 'typeorm';
-import { AppLoggerModule } from './logger/logger.module';
-import { NotificationsModule } from './notifications/notifications.module';
-import { AppEventsModule } from './app-events/app-events.module';
+import { DatabaseModule } from './database';
+import { AppConfigModule } from './config';
+import { AuthModule } from './auth';
+import { UsersModule } from './users';
+import { AppLoggerModule } from './logger';
+import { NotificationsModule } from './notifications';
+import { AppEventsModule } from './app-events';
 
 @Module({
     imports: [
