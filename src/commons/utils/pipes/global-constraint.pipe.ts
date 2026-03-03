969f33574100a195d88c91ea0db176ef638de29a
JulianTs0 <juliantorresssr@gmail.com>
2026-03-03 18:53:15 -0300

    fix: validator config && add: logs

--- src/commons/utils/pipes/global-constraint.pipe.ts
+++ src/commons/utils/pipes/global-constraint.pipe.ts
@@ -1,16 +1,32 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ValidationError } from 'class-validator';
 import { Errors } from '../../error/error-type.constants';
 import { ServiceError } from '../../error/service.error';
 
 export class GlobalConstraintHandler extends ValidationPipe {
+    private readonly logger = new Logger(
+        GlobalConstraintHandler.name,
+    );
     constructor() {
         super({
             whitelist: true,
             transform: true,
-            forbidNonWhitelisted: true,
-            validateCustomDecorators: true,
+            forbidNonWhitelisted: false,
+            validateCustomDecorators: false,
             exceptionFactory: (errors: ValidationError[]) => {
+                const formattedErrors = errors
+                    .map((err) => {
+                        const failedRules = Object.keys(
+                            err.constraints || {},
+                        ).join(', ');
+                        return `${err.property}: [${failedRules}]`;
+                    })
+                    .join(' | ');
+
+                this.logger.warn(
+                    `Invalid fields: ${formattedErrors}`,
+                );
+
                 const allConstraints = errors
                     .map((error) => error.constraints)
                     .filter((constraint) => constraint !== undefined)
